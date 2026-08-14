d1b045381670e711830873f98a63ec86e6b7ec53
elenilson-hora <nino_lindo10@outlook.com>
2026-08-14 01:19:32 -0300

    Basic Menu P1

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Fusion;
+
+public class MainMenu : MonoBehaviour
+{
+    // Input System
+    private InputAction clickAction;
+
+    // Unity Engine
+    public GameObject menuClick;
+    public GameObject menuPrincipal;
+
+    public GameObject buttonJogar;
+    
+    private void Start()
+    {
+        clickAction = InputSystem.actions.FindAction("UI/Click"); // Click contem touch press
+    }
+
+    private void Update()
+    {
+        // Vericicando o click para desabilitar o Menu Click 
+        if (menuClick.activeSelf && clickAction.IsPressed())
+        {
+            menuClick.SetActive(false);
+        }
+    }
+
+    public void OnButtonJogar()
+    {
+        menuPrincipal.SetActive(false);
+        buttonJogar.SetActive(false);
+    }
+}
